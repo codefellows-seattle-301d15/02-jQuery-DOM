b9db2c797b45c066343dfdb7be0088b7bc05626b
Rick Patci <rpatci@gmail.com>
2016-06-07 07:16:42 -0700

    push starter-code class-02

---
+++ starter-code/scripts/article.js
@@ -0,0 +1,35 @@
+var articles = [];
+
+function Article (opts) {
+  // TODO: Use the object passed in to complete this contructor function:
+  // Save ALL the properties of `opts` into `this`.
+
+}
+
+// Define a method on the Article prototype
+
+  /* TODO: Now use jQuery to fill in the rest of the current
+  template clone with properties from this particular Article instance.
+  We need to fill in:
+   1. author name,
+   2. author url,
+   3. article title,
+   4. article body, and
+   5. publication date. */
+
+  // Display the date as a relative number of "days ago":
+
+
+  /* TODO: This cloned article is no longer a template,
+   as it now has real data attached to it! We need to account
+   for that and change it before this current article gets
+   rendered to our DOM. */
+
+
+// Sort our data by date published, descending order
+
+/* Now iterate through our transformed collection and instantiate
+ a new Article instance for each object in our collection. */
+
+/* Append each Article to the DOM.
+NOTE: Remember that the '.toHtml' method invoked is one WE created. */
