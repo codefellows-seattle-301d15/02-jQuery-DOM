7d38045568c56ea277708b5756c06c69950c1846
Rick Patci <rpatci@gmail.com>
2016-07-12 15:45:36 -0700

    push lecture code to day-02

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -3,5 +3,41 @@ var articles = [];
 function Article (opts) {
   // TODO: Use the object passed in to complete this constructor function:
   // Save ALL the properties of `opts` into `this`.
-
+  this.author = opts.author;
 }
+
+Article.prototype.toHtml = function() {
+  var $newArticle = $('article.template').clone();
+  $newArticle.attr('data-category', this.category);
+
+  /* TODO: Now use jQuery to fill in the rest of the current
+  template clone with properties from this particular Article instance.
+  We need to fill in:
+    1. author name,
+    2. author url,
+    3. article title
+    4. article body, and
+    5. publication date. */
+
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+
+  /* TODO: This clone article is no longer a template,
+  as it now has real data attached to it! We need to account
+  for that before this current article gets rendered to our
+  DOM. */
+
+  return $newArticle;
+};
+
+ourLocalData.sort(function(curElem, nextElem) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+ourLocalData.forEach(function(ele) {
+  articles.push(new Article(ele));
+});
+
+articles.forEach(function(a) {
+  $('#articles').append(a.toHtml());
+});
