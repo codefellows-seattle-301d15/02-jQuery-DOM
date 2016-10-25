8cef2cc86a5e7e02e0f16036f3d6b65ca983dfd1
Rick Patci <rpatci@gmail.com>
2016-10-25 15:51:37 -0700

    lecture code push

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -7,13 +7,22 @@ function Article (options) {
    a placeholder for the object that will ultimately be
    passed in. Use all of the properties in blogArticles
    to populate the new Article data that we'll use.  */
+   this.title = options.title;
 };
 
 Article.prototype.toHtml = function() {
-
+  var $newArticle = $('article.template').clone();
+  $newArticle.attr('data-category', this.category);
+  /* TODO: We also need to fill in:
+  1. author name
+  2. author url
+  3. article title
+  4. article body
+  5. publication*/
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-
+/* TODO: This cloned article is no longer a template, as it now
+has real data attached to it. Remove the class from this new article! */
   return $newArticle;
 };
 
@@ -29,4 +38,8 @@ blogArticles.sort(function(currentObject, nextObject) {
 
 blogArticles.forEach(function(ele) {
   articles.push(new Article(ele));
+});
+
+articles.forEach(function(article) {
+  $('#articles').append(article.toHtml());
 });
