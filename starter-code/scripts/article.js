ae7dc7f0f102beafe7e099628ad8dba48f9ddf21
Geno Guerrero <geno.guerrero@gmail.com>
2016-11-29 15:34:19 -0800

    finished the first two TODOs

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,13 +1,18 @@
 var articles = [];
 
 function Article (options) {
-/* TODO: This is our Model constructor. It will take in
+/* TODONE: This is our Model constructor. It will take in
    our source data from blogArticles and instantiate a
    new Object according to this new definition. options is
    a placeholder for the object that will ultimately be
    passed in. Use all of the properties in blogArticles
    to populate the new Article data that we'll use.  */
-   this.title = options.title;
+  this.title = options.title;
+  this.category = options.category;
+  this.author = options.author;
+  this.authorUrl = options.authorUrl;
+  this.publishedOn = options.publishedOn;
+  this.body = options.body;
 };
 
 /* This toHtml function is one that we are creating, and it is
