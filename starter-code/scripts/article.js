1b6f87de1cf8d2fb1d18e287fb9654c63ee7566a
mikemiksch <mikemiksch@gmail.com>
2016-11-29 15:59:59 -0800

    Got almost everything working except the body text, working on debugging

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -20,17 +20,17 @@ function Article (options) {
    being attached to the prototype. Why? */
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
+  /* TODONE: We also need to fill in:*/
   $newArticle.attr('data-category', this.category);
-  /* TODO: We also need to fill in:
-  1. author name
-  2. author url
-  3. article title
-  4. article body
-  5. publication*/
+  $newArticle.find('h1').text(this.title);
+  $newArticle.attr('href', this.authorUrl);
+  $newArticle.find('a').text(this.author);
+  $newArticle.attr('article-body', this.body);
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
 /* TODO: This cloned article is no longer a template, as it now
 has real data attached to it. Remove the class from this new article! */
+  $newArticle.removeClass('template');
   return $newArticle;
 };
 
