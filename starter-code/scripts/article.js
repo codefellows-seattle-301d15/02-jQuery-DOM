c0ce68416b695afe44ebcafd22cf655bdd1d3dab
sueanyv <valenciasue@hotmail.com>
2016-11-29 16:46:37 -0800

    changes to html and js

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,12 +1,12 @@
 var articles = [];
 
-function Article (options) {
-/* TODONE: This is our Model constructor. It will take in
-   our source data from blogArticles and instantiate a
-   new Object according to this new definition. options is
-   a placeholder for the object that will ultimately be
-   passed in. Use all of the properties in blogArticles
-   to populate the new Article data that we'll use.  */
+function Article(options) {
+  /* TODONE: This is our Model constructor. It will take in
+     our source data from blogArticles and instantiate a
+     new Object according to this new definition. options is
+     a placeholder for the object that will ultimately be
+     passed in. Use all of the properties in blogArticles
+     to populate the new Article data that we'll use.  */
   this.title = options.title;
   this.category = options.category;
   this.author = options.author;
@@ -20,16 +20,22 @@ function Article (options) {
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.attr('data-category', this.category);
+  $newArticle.find('.article-body').html(this.body);
+  $newArticle.find('h1').html(this.title);
+  $newArticle.find('a').html(this.author).attr('href', this.authorUrl);
+
+
   /* TODO: We also need to fill in:
-  1. author name
+$newArticle.attr('data-category', this.category);
   2. author url
   3. article title
   4. article body
   5. publication*/
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-  $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-/* TODO: This cloned article is no longer a template, as it now
-has real data attached to it. Remove the class from this new article! */
+  $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000) + ' days ago');
+  $newArticle.removeClass('template');
+  /* TODO: This cloned article is no longer a template, as it now
+  has real data attached to it. Remove the class from this new article! */
   return $newArticle;
 };
 
